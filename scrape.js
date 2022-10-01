741498aaf8d2a6895611ed0b276cccea343e42fd
Apollo1291 <hemphilled@icloud.com>
2022-10-01 15:00:15 -0400

    use cases

--- scrape.js
+++ scrape.js
@@ -1,8 +1,30 @@
 
+const collectProductData = () =>{
+    companyName;
+    productWeight;
+    shippingFrom;
+    merchantCountry
 
+// get Brand name  
 if (document.getElementById('bylineInfo')){
-    
-Company_name = document.getElementById('bylineInfo').textContent.slice(10)
 
-console.log(Company_name)
+tentativeName = document.getElementById('bylineInfo').textContent
+if(tentativeName.includes("Visit the")){
+    companyName = tentativeName
+    .slice(10)
+}
+else if(tentativeName.includes("Brand: ")){
+    console.log('as')
+    companyName = tentativeName
+    .slice(
+        tentativeName.indexOf(':')
+    )   
+}
+
+// get Weight, if no weight get dimensions. 
+
+console.log(companyName)
+
+}
+
 }
