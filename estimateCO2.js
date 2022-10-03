dfd4ec7b54218d5edbc08c3efed5e239eb45f44c
Apollo1291 <hemphilled@icloud.com>
2022-10-03 00:18:38 -0400

    collectTitle

---
+++ estimateCO2.js
@@ -0,0 +1,11 @@
+
+My_API_KEY
+const getEstimate = () => {
+
+    fetch("https://beta3.api.climatiq.io/estimate",{
+        method: 'POST',
+        headrs: {
+            Authorization: Bearer 
+        }
+    } )
+}
--- scrape.js
+++ scrape.js
@@ -1,34 +1,54 @@
+const collectPodructCompany = () =>{
 
+    if (document.getElementById('bylineInfo')){
+
+        tentativeName = document.getElementById('bylineInfo').textContent
+        if(tentativeName.includes("Visit the")){
+            companyName = tentativeName
+            .slice(10)
+        }
+        else if(tentativeName.includes("Brand: ")){
+            console.log('as')
+            companyName = tentativeName
+            .slice(
+                tentativeName.indexOf(':')
+            )   
+        }
+}
+
+return companyName
+}
+
+const collectProductTitle = () => {
+    Title = document.getElementById("productTitle")
+    if (Title){
+        productTitle = Title.textContent
+    }
+
+    return productTitle
+}
 const collectProductData = () =>{
     procuctInfo = {
     companyName: "",
+    procuctTitle: "",
     productWeight: "",
     shippingFrom: "",
-    merchantCountry: ""
+    merchantCountry: "",
 }
 
 // get Brand name  
-if (document.getElementById('bylineInfo')){
+    procuctInfo.companyName = collectPodructCompany()
 
-tentativeName = document.getElementById('bylineInfo').textContent
-if(tentativeName.includes("Visit the")){
-    companyName = tentativeName
-    .slice(10)
-}
-else if(tentativeName.includes("Brand: ")){
-    console.log('as')
-    companyName = tentativeName
-    .slice(
-        tentativeName.indexOf(':') + 2
-    )   
-}
+// Get product Title for AI categorization
+    procuctInfo.productTitle = 
 
 // get Weight, if no weight get dimensions. 
 
+console.log(collectPodructTitle())
 console.log(companyName)
 
 }
 
-}
+
 
 collectProductData()
