9c914ae6ea65038fef5c8f2ec75612857af75eef
Apollo1291 <hemphilled@icloud.com>
2022-10-09 13:49:18 -0400

    Maps categories from amazon to api required

---
+++ CategoryMapping.py
@@ -0,0 +1,103 @@
+#constant
+AMAZON_TO_API_CATEGORY = {
+    'Sports & Outdoors': "Sporting and athletic goods",
+    'Clothing, Shoes & Jewelry': "Clothing",
+    'Home & Kitchen': "Home cooking appliances",
+    'Baby Products': "Consumer goods and general rental centers",
+    'Pet Supplies': "All other retail",
+    'Arts, Crafts & Sewing': "Independent artists/writers and performers",
+    'Electronics': "other miscellaneous electrical equipment and components",
+    'Video Games': "Computer software and games",
+    'Patio, Lawn & Garden': "Building material and garden equipment and supplies dealers",
+    'Tools & Home Improvement': "Power tools",
+    'Office Products': "Office supplies (not paper)",
+    'Grocery & Gourmet Food': "Grocery and related product wholesalers",
+    'Remote & App Controlled Vehicle Parts': "Sale/maintenance/repair/parts/accessories of motor vehicles/motorcycles",
+    'Health & Household': "Health and personal care stores",
+    'Industrial & Scientific': "Other commercial and service industry machinery",
+    'Beauty & Personal Care': "Health and personal care stores",
+    'Hobbies': "General merchandise stores",
+    'Remote & App Controlled Vehicles & Parts': "Sale/maintenance/repair/parts/accessories of motor vehicles/motorcycles",
+    'Automotive': "Automobiles",
+    'Musical Instruments': "Gaskets/seals/musical instruments/fasteners/brooms/brushes/mop and other misc. goods",
+    'Movies & TV': "Radio and television",
+    'Cell Phones & Accessories': "Telephones",
+    'Toys & Games': "Dolls/toys and games",
+    'Appliances': "Small electrical appliances",
+    'Books': "Books",
+    'Mobility & Daily Living Aids': "All other retail",
+    'Power & Hand Tools': "Power tools",
+    'Medical Supplies & Equipment':  "Medical/precision and optical instruments/watches and clocks",
+    'Kitchen & Dining': "Domestic appliances and disposables",
+    'Motorcycle & Powersports': "Motorcycle/bicycle and parts",
+    'CDs & Vinyl': "External hard drives/CDs/other storage media",
+    'Small Appliance Parts & Accessories': "Small electrical appliances",
+    'Office Electronics': "Office machinery and computers",
+    'Instrument Accessories': "Gaskets/seals/musical instruments/fasteners/brooms/brushes/mop and other misc. goods"
+
+}
+
+API_CATEGORY_TO_EMISSION_FACTOR_ID = {
+    "Sporting and athletic goods": "60e77058-39ec-4cbb-9fd7-1518936138f3",
+
+    "Clothing": "0c3fd8c6-7b47-4607-92eb-5097e1dd58cb",
+    
+    "Home cooking appliances": "1e622044-56eb-4b3e-b7db-28121de61c5e",
+
+    "Consumer goods and general rental centers": "c23ad7b2-2e83-4e66-b2c3-6402c62a3246",
+    
+    "other miscellaneous electrical equipment and components": "6d1547d9-404b-440f-8e7b-8dbebfecbbad",
+
+    "Computer software and games": "e590de48-217d-4c61-a465-45b878dd049e",
+
+    "Independent artists/writers and performers": "45eb3b01-dae4-42b9-be7a-577d53e408a3",
+
+    "Health and personal care stores": "2a4974a2-4e74-4e98-aa7e-45822c1177a3",
+
+    "Building material and garden equipment and supplies dealers": "33c087e6-f075-4779-963e-5c21ab7a212d",
+
+    "Power tools": "d680d03b-63d5-4a59-9da0-c67b5216a4e6",
+
+    "Office supplies (not paper)": "b21b5abe-aa38-4841-9a98-6b76cb7d532e",
+
+    "Grocery and related product wholesalers": "afa7587e-967c-43e6-90f6-ba7ba203b210",
+
+    "Other commercial and service industry machinery": "6b563e43-e0e6-469c-beda-b35693991688",
+
+    "Sale/maintenance/repair/parts/accessories of motor vehicles/motorcycles": "7cb9576b-f21b-4bfc-995e-c0c89f5efe0d",
+
+    "General merchandise stores": "58cff1ad-a620-4256-8cd0-c77e791c8a7d",
+
+    "Automobiles": "760df9e0-8509-4fa3-93f3-383831f07292",
+
+    "Gaskets/seals/musical instruments/fasteners/brooms/brushes/mop and other misc. goods": "56e13dfe-0bca-44b9-a04f-924a50710734",
+
+    "Radio and television": "2a4d169a-0a9d-4370-95ed-8d7eff72f068",
+
+    "Telephones": "bc6bf6c9-d5df-41c0-9e77-380ebf38c87c",
+
+    "Dolls/toys and games": "232ddd4d-2850-41c5-910a-635daf2422ac",
+
+    "Small electrical appliances": "1405f53f-fbfc-47ff-9c75-d2d35c8c7596",
+
+    "Books": "b3b37672-39e8-466e-9058-033f8ab40d07",
+
+    "All other retail": "7a384da9-ad84-44bc-a252-768e516a5181",
+
+    "Medical/precision and optical instruments/watches and clocks": "8041917e-cb86-42a2-8b82-87a18e1bb288",
+
+    "External hard drives/CDs/other storage media": "1a49bb56-ddeb-4675-9f1d-c59745eb2501",
+
+    "Motorcycle/bicycle and parts": "a54069cb-d90f-4422-bb63-fcc67e188142",
+
+    "Office machinery and computers": "3d009931-563b-4346-9403-bfb6f0461010"
+
+
+
+
+
+
+
+
+}
+
