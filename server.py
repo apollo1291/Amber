b4f2e57d6af91c51c3cf51b482884f45e323fbc9
Apollo1291 <hemphilled@icloud.com>
2022-10-09 14:58:08 -0400

    work

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ def start():
 def process_json():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        json = request
+        json = request.get_json()
         
         return json
     else:
