bd8af4deb80b9b2ca12bf6303bb5af8bd5489d19
Apollo1291 <hemphilled@icloud.com>
2022-10-09 14:42:33 -0400

    cors

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 # import main Flask class and request object
 from flask import Flask, request
+from flask_cors import CORS
 
-# create the Flask app
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def start():
