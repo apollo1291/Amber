6a4e4e7413ce4743cd53b0668ac61274a26fef45
Apollo1291 <hemphilled@icloud.com>
2022-10-11 13:56:16 -0400

    output category not vector

--- emissions_calculator/usableModel.py
+++ emissions_calculator/usableModel.py
@@ -8,4 +8,4 @@ def predict(text, text_pipeline):
     with torch.no_grad():
         text = torch.tensor(text_pipeline(text))
         output = model(text, torch.tensor([0]))
-        return output.argmax(1).item() + 1
+        return vec_to_cat[output.argmax(1).item() + 1]
