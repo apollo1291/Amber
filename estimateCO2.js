9e95f3c38ee99df47251776071a97fefaa2d01fe
Apollo1291 <hemphilled@icloud.com>
2022-10-11 12:38:11 -0400

    new usage

--- estimateCO2.js
+++ estimateCO2.js
@@ -35,7 +35,7 @@ fetch("http://localhost:5000/get_emissions", {
     'origin': '*'
 
   },
-  body: { data }
+  body: JSON.stringify( data )
 }).then((serverTest) => {
   serverTest.json().then((serverBody) => {
     console.log(serverBody)
