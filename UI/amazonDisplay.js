324cb8fbf7156d9691a5e91f1e530c9b61f59682
Apollo1291 <hemphilled@icloud.com>
2022-10-21 13:09:31 -0400

    test

--- UI/amazonDisplay.js
+++ UI/amazonDisplay.js
@@ -1,2 +1,4 @@
 var buyButton = document.getElementById("buy-now-button")
 
+emissionData = getEmissions()
+buyButton.textContent = emissionData.emissions.co2e
--- estimateCO2.js
+++ estimateCO2.js
@@ -24,7 +24,7 @@ console.log(emissionData)
 return emissionData
 }
 
-getEmissions()
+
 
 
 
