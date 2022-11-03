b1709ed3081f9145efb2a0c39005108d4c23f707
Apollo1291 <hemphilled@icloud.com>
2022-11-03 00:15:49 -0400

    Added popup

--- UI/amazonDisplay.js
+++ UI/amazonDisplay.js
@@ -2,8 +2,12 @@ const display = async () =>{
 
 var buyButton = document.getElementById("submit.buy-now-announce")
 emissionData = await getEmissions()
-buyButton.textContent = emissionData.emissions.co2e + "kg of Carbon Dioxide"
+buyButton.textContent = emissionData.emissions.co2e + " kg of Carbon Dioxide"
+
+var b = document.getElementsByTagName('body');
+b.innerHTML = b.innerHTML + "<div id=\"display\"><div id = \"header\">Amber</div><div id = \"main\">Buyers Impact: 2 Kilos of Carbon Dioxide</div></div>";
 
 }
 
 display()
+
